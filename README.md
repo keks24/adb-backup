556af1c43efb6b19d0b5bcae5d8030ebca195e64
Ramon Fischer <Ramon_Fischer@hotmail.de>
2025-06-14 23:29:13 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,173 @@
+# Introduction
+`adb-backup` saves `partition information` and `various system information` of an `Android device` using `bash` and `adb`.
+
+# Prerequisites
+* `Knowledge` about [`Extended Regular Expressions`](https://en.wikibooks.org/wiki/Regular_Expressions/POSIX-Extended_Regular_Expressions).
+
+* `Advanced knowledge` about the mechanics of `Linux`.
+
+* The following packages are installed:
+```no-highlight
+chmod
+mkdir
+touch
+adb
+b2sum
+gawk
+id
+pixz
+tee
+xz
+```
+
+* The `Smartphone` is connected via `USB`.
+
+* The `Smartphone` is `rebooted` in `recovery` mode and has `ADB enabled`:
+```bash
+$ adb reboot recovery
+$ adb devices -l
+List of devices attached
+<some_device_id>               recovery usb:<some_bus_number>-<some_port_number> product:<some_product_name> model:<some_model_name> device:<some_device_name> transport_id:<some_id>
+```
+
+# Installation
+`Clone` the repository into a directory, where the backup should be saved:
+```bash
+$ git clone "https://codeberg.org/keks24/adb-backup.git"
+```
+
+# Usage
+# Configuration
+Make sure, that the `Android device` is `rebooted` in `recovery mode`.
+
+Adapt the following entries in the configuration file `adb_backup.conf`:
+```no-highlight
+partition_regex="sd[a-f][1-9][0-9]{0,2}"
+adb_device_id="<some_device_id>"
+model_name="model:<some_model_name>"
+device_name="device:<some_device_name>"
+```
+
+Replace the values `<some_device_id>`, `<some_model_name>` and `<some_device_name>` with the information of the `desired Android device`, which should be backed up.
+
+The `Extended Regular Expression` for the variable `partition_regex` needs to be `adapted manually` as well. For this, the partition structure in the file `/proc/partitions` and in the directory `/dev/block/` needs to be analysed:
+```bash
+$ adb -s <some_device_id> shell "head -n 24 '/proc/partitions' | grep -v 'ram'"
+major minor  #blocks  name
+
+   8        0  243539968 sda
+   8        1          8 sda1
+   8        2       8192 sda2
+   8        3      32768 sda3
+   8        4       1024 sda4
+   8        5        512 sda5
+[...]
+$ adb -s <some_device_id> shell "ls -l '/dev/block/'"
+total 0
+lrwxrwxrwx 1 root root 15 1971-12-06 06:12 ALIGN_TO_128K_1 -> /dev/block/sdd1
+lrwxrwxrwx 1 root root 15 1971-12-06 06:12 ALIGN_TO_128K_2 -> /dev/block/sdf1
+[...]
+lrwxrwxrwx 1 root root 16 1971-12-06 06:12 android_log -> /dev/block/sde79
+[...]
+```
+
+In this case the desired `block device files`, which contain the `partition information`, are:
+```no-highlight
+/dev/block/sda1
+/dev/block/sda2
+/dev/block/sdb1
+[...]
+/dev/block/sdf1
+[...]
+```
+
+In order match `all` these files, the following `Extended Regular Expression` can be used:
+```bash
+## partitions: sd{a..f}{1..999}
+partition_regex="sd[a-f][1-9][0-9]{0,2}"
+```
+
+This will match `all block device files` from:
+* `/dev/block/sda1` to `/dev/block/sda999`
+* `/dev/block/sdb1` to `/dev/block/sdb999` and so on until
+* `/dev/block/sdf1` to `/dev/block/sdf999`
+
+Matching files until `999`, makes the script dynamic for future updates.
+
+Another example with multiple `SD cards`:
+```bash
+## partitions: mmcblk{0..2}{1..999}
+partition_regex="mmcblk[0-2]p[1-9][0-9]{0,2}"
+```
+
+The online tool [`RegExr`](https://regexr.com/) can be used for debugging.
+
+# Further configuration
+The array `system_information_array` can be adapted, in order to backup more files.
+
+# Execution
+Once everything is configured properly, the `Bash` script `adb_backup.sh` can now be executed:
+```bash
+$ adb_backup.sh
+```
+
+This will create a `new backup directory`, in which the `entire backup process` and `errors` will be `logged`:
+```bash
+2025-06-14T19-36-30+0200: <some_device_id>: Saving file: '/etc/blkid.tab' to './2025-06-14T19-36-29+0200_backup//etc/blkid.tab'...
+/etc/blkid.tab: 1 file pulled, 0 skipped. 0.0 MB/s (693 bytes in 0.042s)
+2025-06-14T19-36-30+0200: <some_device_id>: Saving file: '/etc/fstab' to './2025-06-14T19-36-29+0200_backup//etc/fstab'...
+/etc/fstab: 1 file pulled, 0 skipped. 0.0 MB/s (747 bytes in 0.043s)
+2025-06-14T19-36-30+0200: <some_device_id>: Saving file: '/etc/recovery.fstab' to './2025-06-14T19-36-29+0200_backup//etc/recovery.fstab'...
+/etc/recovery.fstab: 1 file pulled, 0 skipped. 1.2 MB/s (5557 bytes in 0.004s)
+2025-06-14T19-36-30+0200: <some_device_id>: Saving file: '/proc/cmdline' to './2025-06-14T19-36-29+0200_backup//proc/cmdline'...
+/proc/cmdline: 1 file pulled, 0 skipped. 0.4 MB/s (1884 bytes in 0.005s)
+2025-06-14T19-36-30+0200: <some_device_id>: Saving file: '/proc/config.gz' to './2025-06-14T19-36-29+0200_backup//proc/config.gz'...
+/proc/config.gz: 1 file pulled, 0 skipped. 5.8 MB/s (41007 bytes in 0.007s)
+2025-06-14T19-36-30+0200: <some_device_id>: Saving file: '/proc/cpuinfo' to './2025-06-14T19-36-29+0200_backup//proc/cpuinfo'...
+/proc/cpuinfo: 1 file pulled, 0 skipped. 0.4 MB/s (1896 bytes in 0.004s)
+2025-06-14T19-36-30+0200: <some_device_id>: Saving file: '/proc/devices' to './2025-06-14T19-36-29+0200_backup//proc/devices'...
+/proc/devices: 1 file pulled, 0 skipped. 0.2 MB/s (1021 bytes in 0.004s)
+2025-06-14T19-36-30+0200: <some_device_id>: Saving file: '/proc/meminfo' to './2025-06-14T19-36-29+0200_backup//proc/meminfo'...
+/proc/meminfo: 1 file pulled, 0 skipped. 0.3 MB/s (1093 bytes in 0.004s)
+2025-06-14T19-36-30+0200: <some_device_id>: Saving file: '/proc/partitions' to './2025-06-14T19-36-29+0200_backup//proc/partitions'...
+/proc/partitions: 1 file pulled, 0 skipped. 1.0 MB/s (4311 bytes in 0.004s)
+2025-06-14T19-36-30+0200: <some_device_id>: Saving partition labels: from '/dev/block/by-name/' to './2025-06-14T19-36-29+0200_backup//dev/block/partition_labels.list'...
+:
+2025-06-14T19-36-30+0200: <some_device_id>: Saving block device: '/dev/block/sda1' to './2025-06-14T19-36-29+0200_backup/sda1.img'...
+[...]
+2025-06-14T21-28-14+0200: <some_device_id>: Compressing file: './2025-06-14T19-36-29+0200_backup/sda1.img' to './2025-06-14T19-36-29+0200_backup/sda1.img.xz'...
+[...]
+2025-06-14T22-22-04+0200: <some_device_id>: Checking archive integrity of: './2025-06-14T19-36-29+0200_backup/sda1.img.xz
+2025-06-14T23-37-41+0200: <some_device_id>: Generating BLAKE2 checksum file: './2025-06-14T19-36-29+0200_backup/sda1.img.xz.b2' of './2025-06-14T19-36-29+0200_backup/sda1.img'...
+$ LS_COLORS="" tree -FC "./2025-06-14T19-36-29+0200_backup/"
+2025-06-14T19-36-29+0200_backup/
+├── 2025-06-14T19-36-29+0200_backup.err
+├── 2025-06-14T19-36-29+0200_backup.log
+├── dev/
+│   └── block/
+│       └── partition_labels.list
+├── etc/
+│   ├── blkid.tab
+│   ├── fstab
+│   └── recovery.fstab
+├── proc/
+│   ├── cmdline
+│   ├── config.gz
+│   ├── cpuinfo
+│   ├── devices
+│   ├── meminfo
+│   └── partitions
+├── sda1.img.xz
+├── sda1.img.xz.b2
+[...]
+```
+
+# Verifying archive and file integrity
+The following commands can be used to `verify` the `archive` and `file` integrity:
+```bash
+$ xz --test --verbose "./2025-06-14T19-36-29+0200_backup/"sd*.xz
+$ b2sum --check "./2025-06-14T19-36-29+0200_backup/"*.b2
+```
+
+# Parameters
+There are `no parameters`.
---
+++ adb_backup.sh
@@ -0,0 +1,413 @@
+#!/bin/bash
+############################################################################
+# Copyright 2025 Ramon Fischer                                             #
+#                                                                          #
+# Licensed under the Apache License, Version 2.0 (the "License");          #
+# you may not use this file except in compliance with the License.         #
+# You may obtain a copy of the License at                                  #
+#                                                                          #
+#     http://www.apache.org/licenses/LICENSE-2.0                           #
+#                                                                          #
+# Unless required by applicable law or agreed to in writing, software      #
+# distributed under the License is distributed on an "AS IS" BASIS,        #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. #
+# See the License for the specific language governing permissions and      #
+# limitations under the License.                                           #
+############################################################################
+
+# exit immediately on pipe errors
+set -o pipefail
+
+# be able to kill the script gracefully within functions
+## on "SIGTERM" signal, execute "exit 1"
+trap "exit 1" SIGTERM
+
+# define global variables
+script_directory_path="${0%/*}"
+script_name="${0##*/}"
+# make script pid available for subshells
+export script_pid="${$}"
+configuration_name="${script_name/\.sh/.conf}"
+configuration_file="${script_directory_path}/${configuration_name}"
+source "${script_directory_path}/${script_name/\.sh/.conf}"
+
+# secure access permissions
+## created files: 600
+## created directories: 700
+umask "077"
+
+createBackupDirectories()
+{
+    /bin/mkdir \
+        --parent \
+        "${backup_directory}" \
+        "${backup_directory}/dev" \
+        "${backup_directory}/"{dev/block,etc,proc}
+}
+
+createBackupDirectories
+
+createLogFiles()
+{
+    declare -a file_array
+    file_array=(\
+                "${log_file}" \
+                "${error_log_file}" \
+               )
+    local file
+
+    for file in "${file_array[@]}"
+    do
+        /bin/touch "${file}"
+        /bin/chmod 600 "${file}"
+    done
+}
+
+createLogFiles
+
+declare -a command_array
+command_array=(\
+                "/bin/chmod" \
+                "/bin/mkdir" \
+                "/bin/touch" \
+                "/usr/bin/adb" \
+                "/usr/bin/b2sum" \
+                "/usr/bin/gawk" \
+                "/usr/bin/id" \
+                "/usr/bin/pixz" \
+                "/usr/bin/tee" \
+                "/usr/bin/xz" \
+              )
+checkCommands()
+{
+    local current_command
+
+    unalias "${command_array[*]##*/}" 2>/dev/null
+
+    {
+        for current_command in "${command_array[@]}"
+        do
+            if ! command -v "${current_command}" >/dev/null
+            then
+                echo -e "\e[01;31mCould not find command '${current_command}'.\e[0m" >&2
+                kill -s "SIGTERM" "${script_pid}"
+            fi
+        done
+    # needs to be done manually, because of dependency conflicts.
+    } > >(/usr/bin/tee --append "${log_file}" >&1) 2> >(/usr/bin/tee --append "${error_log_file}" >&2)
+}
+
+checkCommands
+
+# define more global variables
+## "checkCommands" must be executed before this!
+effective_username=$(/usr/bin/id --user --name)
+adb_command_output=$(/usr/bin/adb devices -l)
+
+# global functions
+outputWarningError()
+{
+    local message="${1}"
+    local message_type="${2}"
+
+    case "${message_type}" in
+        "warning")
+            echo -e "${date_time_format@P}: \e[01;33m${adb_device_id}: ${message}\e[0m" | /usr/bin/tee --append "${error_log_file}" >&2
+            ;;
+
+        "error")
+            echo -e "${date_time_format@P}: \e[01;31m${adb_device_id}: ${message}\e[0m" | /usr/bin/tee --append "${error_log_file}" >&2
+            ;;
+
+        *)
+            # no coloured output
+            echo -e "${date_time_format@P}: ${adb_device_id}: ${message}" | /usr/bin/tee --append "${error_log_file}" >&2
+            ;;
+    esac
+}
+
+writeLogFile()
+{
+    local function_name="${0}"
+    local log_type="${1}"
+
+    case "${log_type}" in
+        "log")
+            if [[ -O "${log_file}" && -r "${log_file}" && -w "${log_file}" ]]
+            then
+                /usr/bin/tee --append "${log_file}" >&1
+            else
+                outputWarningError "The log file: '${log_file}' is either not owned by effective user '${effective_username}', has no 'read' or 'write' permissions." "error"
+                kill -s "SIGTERM" "${script_pid}"
+            fi
+            ;;
+        "error")
+            if [[ -O "${log_file}" && -r "${log_file}" && -w "${log_file}" ]]
+            then
+                /usr/bin/tee --append "${error_log_file}" >&2
+            else
+                outputWarningError "The error log file: '${log_file}' is either not owned by effective user '${effective_username}', has no 'read' or 'write' permissions." "error"
+                kill -s "SIGTERM" "${script_pid}"
+            fi
+            ;;
+        *)
+            outputWarningError "${function_name}: Wrong parameter: Must be either 'log' or 'error'." "error"
+            kill -s "SIGTERM" "${script_pid}"
+    esac
+}
+
+
+outputNewline()
+{
+    echo "${log_date_time_format@P}:" | /usr/bin/tee --append "${log_file}"
+}
+
+outputCurrentStep()
+{
+    local message="${*}"
+
+    {
+        echo -e "${date_time_format@P}: \e[01;34m${adb_device_id}: ${message}\e[0m"
+    } > >(writeLogFile "log") 2> >(writeLogFile "error")
+}
+
+getPartitionList()
+{
+    local partition_file="${backup_directory}/${partition_table_file}"
+
+    if [[ -f "${partition_file}" ]]
+    then
+        # get partition information
+        local partition_table=$(< "${partition_file}")
+        # filter list of partition names
+        ## make it globally available
+        partition_name_list=$(/usr/bin/gawk \
+                                --assign="partition_regex=${partition_regex}" \
+                                '$0 ~ partition_regex { print $4 }' <<< "${partition_table}")
+        echo "${partition_name_list}"
+    else
+        outputWarningError "Could not find file: '${partition_file}'" "error"
+        kill -s "SIGTERM" "${script_pid}"
+    fi
+}
+
+executeArchiveCommand()
+{
+    local file="${1}"
+
+    {
+        /usr/bin/pixz -9 "${file}"
+    } > >(writeLogFile "log") 2> >(writeLogFile "error")
+}
+
+executeArchiveCheckCommand()
+{
+    declare -a compressed_file_array
+    compressed_file_array=("${@}")
+
+    {
+        # TODO: parallelise this with "xargs"
+        /usr/bin/xz --test "${compressed_file_array[@]}"
+    } > >(writeLogFile "log") 2> >(writeLogFile "error")
+}
+
+executeChecksumCommand()
+{
+    local compressed_file="${1}"
+    local checksum_file="${2}"
+
+    {
+        /usr/bin/b2sum "${compressed_file}" > "${checksum_file}"
+    } > >(writeLogFile "log") 2> >(writeLogFile "error")
+}
+
+executeAdbCommand()
+{
+    local device_id="${1}"
+    local command_type="${2}"
+    local first_parameter="${3}"
+    local second_parameter="${4}"
+
+    {
+        /usr/bin/adb \
+            -s "${device_id}" \
+            "${command_type}" \
+            "${first_parameter}" \
+            "${second_parameter}"
+    } > >(writeLogFile "log") 2> >(writeLogFile "error")
+}
+
+deviceFileDirectoryExists()
+{
+    local device_file="${1}"
+
+    if executeAdbCommand "${adb_device_id}" "shell" "test -e '${device_file}'" >/dev/null
+    then
+        # exists
+        return 0
+    else
+        # does not exist
+        return 1
+    fi
+}
+
+checkDeviceConnection()
+{
+    local device_information
+
+    if ! /usr/bin/gawk \
+            --assign="adb_device_id=${adb_device_id}" \
+            --assign="transport_protocol=${transport_protocol}" \
+            --assign="boot_mode=${boot_mode}" \
+            --assign="model_name=${model_name}" \
+            --assign="device_name=${device_name}" \
+            '{
+                adb_command_output=$0
+                device_pattern=adb_device_id" +?"boot_mode".?"transport_protocol".?"model_name".*"device_name".*?$"
+
+                if(adb_command_output ~ device_pattern)
+                {
+                    # return true
+                    exit 1
+                }
+                else
+                {
+                    # return false
+                    exit 0
+                }
+             }' <<< "${adb_command_output}"
+    then
+        outputWarningError "The 'ADB device ID' must be: '${adb_device_id}'." "error"
+        outputWarningError "The 'transport protocol' must be: '${transport_protocol}'." "error"
+        outputWarningError "The 'boot mode' must be: '${boot_mode}'." "error"
+        outputWarningError "The 'device name' must be: '${device_name}'." "error"
+        outputWarningError "The 'model name' must be: '${model_name}'." "error"
+        outputWarningError "" "error"
+        outputWarningError "ADB command output (\"/usr/bin/adb devices -l\"):" "warning"
+        outputWarningError "${adb_command_output}"
+        outputWarningError "" "error"
+        outputWarningError "\e[01;33mMake sure to reboot the device to 'recovery mode', in order to create a clean backup of all partitions: \"/usr/bin/adb reboot recovery\"\e[0m" "warning"
+        kill -s "SIGTERM" "${script_pid}"
+    fi
+}
+
+saveSystemInformation()
+{
+    local device_file
+
+    if [[ "${system_information_array[@]}" == *"/proc/partitions"* ]]
+    then
+        for device_file in "${system_information_array[@]}"
+        do
+            if deviceFileDirectoryExists "${device_file}"
+            then
+                outputCurrentStep "Saving file: '${device_file}' to '${backup_directory}/${device_file}'..."
+                executeAdbCommand "${adb_device_id}" "pull" "${device_file}" "${backup_directory}/${device_file}"
+            else
+                outputWarningError "Could not find file on device: '${device_file}'." "error"
+                kill -s "SIGTERM" "${script_pid}"
+            fi
+        done
+    else
+        outputWarningError "Saving the file: '/proc/partitions' is mandatory!" "error"
+        kill -s "SIGTERM" "${script_pid}"
+    fi
+
+    outputCurrentStep "Saving partition labels: from '${partition_label_directory}' to '${partition_label_file}'..."
+    if deviceFileDirectoryExists "${partition_label_directory}"
+    then
+        executeAdbCommand "${adb_device_id}" "shell" "ls -l '${partition_label_directory}'" > "${partition_label_file}"
+    else
+        outputWarningError "Couldn not find directory on device: '${partition_label_directory}'." "error"
+        kill -s "SIGTERM" "${script_pid}"
+    fi
+    outputNewline
+}
+
+savePartitionsAsImages()
+{
+    local partition_name_list=$(getPartitionList)
+    local partition_name
+
+    # save partitions as image files
+    while read -r partition_name
+    do
+        local device_partition_file="${block_device_directory}/${partition_name}"
+        local image_file="${backup_directory}/${partition_name}.img"
+
+        outputCurrentStep "Saving block device: '${device_partition_file}' to '${image_file}'..."
+        # "adb pull" is much faster than "adb shell 'cat [...]'"
+        executeAdbCommand "${adb_device_id}" "pull" "${device_partition_file}" "${image_file}"
+    done <<< "${partition_name_list}"
+
+    outputNewline
+}
+
+archiveImages()
+{
+    local partition_name_list=$(getPartitionList)
+    local partition_name
+
+    while read -r partition_name
+    do
+        local image_file="${backup_directory}/${partition_name}.img"
+        local compressed_image_file="${image_file}.xz"
+
+        outputCurrentStep "Compressing file: '${image_file}' to '${compressed_image_file}'..."
+        executeArchiveCommand "${image_file}"
+    done <<< "${partition_name_list}"
+
+    outputNewline
+}
+
+verifyArchiveIntegrity()
+{
+    local partition_name_list=$(getPartitionList)
+    local partition_name
+    declare -a compressed_image_file_array
+
+    while read -r partition_name
+    do
+        local image_file="${backup_directory}/${partition_name}.img"
+        compressed_image_file_array+=("${image_file}.xz")
+    done <<< "${partition_name_list}"
+
+    outputCurrentStep "Checking archive integrity of: '${compressed_image_file_array[@]}'..."
+    executeArchiveCheckCommand "${compressed_image_file_array[@]}"
+    outputNewline
+}
+
+generateChecksums()
+{
+    local partition_name_list=$(getPartitionList)
+    local partition_name
+
+    while read -r partition_name
+    do
+        local image_file="${backup_directory}/${partition_name}.img"
+        local compressed_image_file="${image_file}.xz"
+        local checksum_file="${compressed_image_file}.b2"
+
+        outputCurrentStep "Generating BLAKE2 checksum file: '${checksum_file}' of '${image_file}'...\e[0m"
+        executeChecksumCommand "${compressed_image_file}" "${checksum_file}"
+    done <<< "${partition_name_list}"
+
+    #outputNewline
+}
+
+main()
+{
+    checkDeviceConnection
+
+    saveSystemInformation
+
+    savePartitionsAsImages
+
+    archiveImages
+
+    verifyArchiveIntegrity
+
+    generateChecksums
+}
+
+main
